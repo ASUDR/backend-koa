9744efc02cca77210328db5fcb3da085c3ef61d8
Sardorkhuja <mr.khuzha@gmail.com>
2021-09-16 01:00:54 +0500

    add Hostel model

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,4 @@
 export { default as sequelize } from './sequelize';
 export * from './models/objects/Country';
 export * from './models/university/Faculty';
+export * from './models/university/Hostel';
---
+++ src/db/models/university/Hostel.ts
@@ -0,0 +1,38 @@
+import Sequelize, { Optional } from 'sequelize';
+import sequelize from '../../sequelize';
+
+const { DataTypes, Model } = Sequelize;
+
+export interface HostelAttributes {
+  id: number;
+  name: string;
+}
+
+export interface HostelAttributesInput extends Optional<HostelAttributes, 'id'> {}
+export interface HostelAttributesOutput extends Required<HostelAttributes> {}
+
+export class Hostel
+  extends Model<HostelAttributes, HostelAttributesInput>
+  implements HostelAttributes {
+  public id: number;
+  public name: string;
+}
+
+Hostel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+
+  name: {
+    type: DataTypes.STRING(32),
+    allowNull: false,
+    unique: true
+  }
+}, {
+  sequelize,
+  schema: 'university',
+  modelName: 'hostels',
+  timestamps: false
+});
