986093b0c64035dabf9699c107488a540a1b1ee1
Sardorkhuja <mr.khuzha@gmail.com>
2021-09-16 01:31:34 +0500

    add Group to export

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,7 @@
 export { default as sequelize } from './sequelize';
 export * from './models/objects/Country';
 export * from './models/university/Faculty';
+export * from './models/university/Group';
 export * from './models/university/Hostel';
 export * from './models/university/Floor';
 export * from './models/university/Room';
