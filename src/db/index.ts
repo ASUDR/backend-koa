5485d7f18be3896c87fd0728f9616a75e97ad08e
Sardorkhuja <mr.khuzha@gmail.com>
2021-09-16 01:19:46 +0500

    add Floor model

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,3 +2,4 @@ export { default as sequelize } from './sequelize';
 export * from './models/objects/Country';
 export * from './models/university/Faculty';
 export * from './models/university/Hostel';
+export * from './models/university/Floor';
---
+++ src/db/models/university/Floor.ts
@@ -0,0 +1,57 @@
+import Sequelize, { Optional } from 'sequelize';
+import sequelize from '../../sequelize';
+import { Hostel } from '../..';
+
+const { DataTypes, Model } = Sequelize;
+
+export interface FloorAttributes {
+  id: number;
+  hostelId: number;
+  number: string;
+}
+
+export interface FloorAttributesInput extends Optional<FloorAttributes, 'id'> {}
+export interface FloorAttributesOutput extends Required<FloorAttributes> {
+  hostel?: Hostel;
+}
+
+export class Floor
+  extends Model<FloorAttributes, FloorAttributesInput>
+  implements FloorAttributes {
+  public id: number;
+  public hostelId: number;
+  public number: string;
+}
+
+Floor.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+
+  hostelId: {
+    type: DataTypes.INTEGER,
+    field: 'hostel_id',
+    allowNull: false
+  },
+
+  number: {
+    type: DataTypes.STRING(16),
+    allowNull: false
+  }
+}, {
+  sequelize,
+  schema: 'university',
+  modelName: 'hostel_floors',
+  timestamps: false
+});
+
+Floor.belongsTo(Hostel, {
+  foreignKey: {
+    name: 'hostelId',
+    allowNull: false
+  },
+  onUpdate: 'CASCADE',
+  onDelete: 'CASCADE'
+});
