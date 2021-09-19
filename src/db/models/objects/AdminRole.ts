50fb5f726306b593b5693c9cf04f1f927e241e5c
Sardorkhuja <mr.khuzha@gmail.com>
2021-09-19 17:55:11 +0500

    add AdminRole model

---
+++ src/db/models/objects/AdminRole.ts
@@ -0,0 +1,38 @@
+import Sequelize, { Optional } from 'sequelize';
+import sequelize from '../../sequelize';
+
+const { DataTypes, Model } = Sequelize;
+
+export interface AdminRoleAttributes {
+  id: number;
+  name: string;
+}
+
+export interface AdminRoleAttributesInput extends Optional<AdminRoleAttributes, 'id'> {}
+export interface AdminRoleAttributesOutput extends Required<AdminRoleAttributes> {}
+
+export class AdminRole
+  extends Model<AdminRoleAttributes, AdminRoleAttributesInput>
+  implements AdminRoleAttributes {
+    id: number;
+    name: string;
+}
+
+AdminRole.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+
+  name: {
+    type: DataTypes.STRING(16),
+    allowNull: false,
+    unique: true
+  }
+}, {
+  sequelize,
+  schema: 'objects',
+  modelName: 'admin_roles',
+  timestamps: false
+});
