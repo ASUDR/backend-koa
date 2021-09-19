ddff1fa30336bd75e7a7d05360a52a4633028f42
Sardorkhuja <mr.khuzha@gmail.com>
2021-09-19 14:43:28 +0000

    add Response class

---
+++ src/utils/returnSchemas/includes/Response.ts
@@ -0,0 +1,6 @@
+export default class Response {
+  constructor(
+    public success: boolean,
+    public data: any
+  ) {}
+}
--- src/utils/returnSchemas/index.ts
+++ src/utils/returnSchemas/index.ts
@@ -2,4 +2,5 @@ export { default as BaseApiError } from './includes/baseApiError';
 export { default as ApiNotFoundError } from './includes/apiNotFoundError';
 export { default as ApiValidationError } from './includes/apiValidationError';
 export { default as BaseApiSchema } from './includes/baseSchema';
+export { default as Response } from './includes/Response';
 export { handleApiError } from './includes/handlers';
