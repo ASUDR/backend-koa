1741cd1f0152d22b7bd7743a960b0653b0bd5c87
Sardorkhuja <mr.khuzha@gmail.com>
2021-09-19 15:01:54 +0000

    minor update utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,7 @@ export * from './returnSchemas';
 export * from './types';
 export * from './validationSchemas';
 export { default as Validator } from './includes/validator';
+export { default as lodgersPassport } from './includes/lodgersPassport';
 export { default as CSRF } from './includes/CSRF';
 export { default as httphttpStatusCodes } from './includes/httpStatusCodes';
 export { default as logger } from './includes/logger';
--- src/utils/types/includes/koa.ts
+++ src/utils/types/includes/koa.ts
@@ -1,6 +1,4 @@
 import Koa from 'koa';
 
 export type Context = Koa.ExtendableContext;
-export type Request = Koa.Request;
-export type Response = Koa.Response;
 export type Next = Koa.Next;
