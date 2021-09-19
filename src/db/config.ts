f10b9d2f4f05e425b9e5466c45318b7f8da50f16
Sardorkhuja <mr.khuzha@gmail.com>
2021-09-19 13:48:30 +0000

    minor update db config

--- src/db/config.ts
+++ src/db/config.ts
@@ -7,11 +7,11 @@ export const config: {
   password: string
 } = {
   dialect: 'postgres',
-  host: process.env.DB_HOST || '',
-  port: +(process.env.DB_PORT || ''),
-  database: process.env.DB_NAME || '',
-  username: process.env.DB_USER || '',
-  password: process.env.DB_PASSWORD || ''
+  host: process.env.DB_HOST!,
+  port: +process.env.DB_PORT!,
+  database: process.env.DB_NAME!,
+  username: process.env.DB_USER!,
+  password: process.env.DB_PASSWORD!
 };
 
 export const connectionString: string = `postgres://${config.username}:${config.password}@${config.host}:${config.port}/${config.database}`;
